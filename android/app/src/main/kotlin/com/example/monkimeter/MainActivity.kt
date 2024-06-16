340a9336ee555139daf826b1f88857b002a73eb3
AlvaroFerGar <alvarofergar95@gmail.com>
2024-06-16 17:01:04 +0200

    Commit inicial

---
+++ android/app/src/main/kotlin/com/example/monkimeter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.monkimeter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+//import 'package:flutter_tts/flutter_tts.dart';
+//import 'package:audioplayers/audioplayers.dart';
+
+
+void main() {
+  runApp(MonkimeterApp());
+}
+
+class MonkimeterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Monkimeter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MonkimeterHomePage(),
+    );
+  }
+}
+
+class MonkimeterHomePage extends StatefulWidget {
+  @override
+  _MonkimeterHomePageState createState() => _MonkimeterHomePageState();
+}
+
+class _MonkimeterHomePageState extends State<MonkimeterHomePage> {
+  int _seconds = 0;
+  Timer? _timer;
+  bool _isHanging = false;
+  //FlutterTts flutterTts = FlutterTts();
+  //AudioPlayer audioPlayer = AudioPlayer();
+
+  void _startCountdown() {
+    setState(() {
+      _seconds = 5;  // Inicia el contador en 5 segundos
+      _isHanging = true;  // Cambia el estado a colgado (hang)
+    });
+
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_seconds > 0) {
+          _seconds--;  // Decrementa el contador cada segundo
+          _playAudio('beep.mp3'); // Reproduce el sonido de beep cada segundo
+        } else {
+          _timer?.cancel();  // Cancela el temporizador de la cuenta regresiva
+          _startTimer();  // Inicia el temporizador principal después de la cuenta regresiva
+        }
+      });
+    });
+  }
+
+  void _startTimer() {
+    // Aquí se inicia el temporizador principal
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds++;
+        //_speak('${_seconds + 1}'); // Anuncia el número (1, 2, 3, etc.)
+      });
+    });
+  }
+
+/*  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage("es-ES");
+    await flutterTts.setSpeechRate(1.0);
+    await flutterTts.setPitch(1.0);
+    await flutterTts.speak(text);
+  }
+*/
+
+  Future<void> _playAudio(String audioPath) async {
+  try {
+    //await audioPlayer.setSource(AssetSource(audioPath));
+    //await audioPlayer.resume();
+    // Puedes añadir código aquí para manejar el éxito de la reproducción
+    print('Audio playing successfully');
+  } catch (e) {
+    // Manejo de errores, si ocurre alguno
+    print('Error playing audio: $e');
+  }
+  }
+
+  void _stopTimer() {
+    _timer?.cancel();
+    setState(() {
+      _isHanging = false;
+    });
+    // Aquí se guarda el tiempo de colgado en la base de datos
+    _saveHang(_seconds);
+  }
+
+  Future<void> _saveHang(int duration) async {
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'hangboard_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE hangs(id INTEGER PRIMARY KEY, duration INTEGER, date TEXT)",
+        );
+      },
+      version: 1,
+    );
+
+    final db = await database;
+    await db.insert(
+      'hangs',
+      {'duration': duration, 'date': DateTime.now().toString()},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Monkimeter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '🦧 Tiempo colgado 🦧',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Text(
+              '$_seconds segundos',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            SizedBox(height: 20),
+            _isHanging
+                ? ElevatedButton(
+                    onPressed: _stopTimer,
+                    child: Text('Detener'),
+                  )
+                : ElevatedButton(
+                    onPressed: _startCountdown,  // Cambiado a _startCountdown
+                    child: const Text('Iniciar'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
